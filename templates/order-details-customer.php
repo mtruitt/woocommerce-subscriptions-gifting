052faa52627969bd3d0ae2b4c31d966f32441631
LCmry <lcrees.2014@gmail.com>
2018-02-07 16:10:06 -0800

    Style and formatting fixes

--- templates/order-details-customer.php
+++ templates/order-details-customer.php
@@ -6,18 +6,19 @@ if ( ! defined( 'ABSPATH' ) ) {
 ?>
 <section class="woocommerce-customer-details">
 
-		<section class="woocommerce-columns woocommerce-columns--2 woocommerce-columns--addresses col2-set addresses">
+	<section class="woocommerce-columns woocommerce-columns--2 woocommerce-columns--addresses col2-set addresses">
 
-			<div class="woocommerce-column woocommerce-column--1 woocommerce-column--billing-address col-1">
+		<div class="woocommerce-column woocommerce-column--1 woocommerce-column--billing-address col-1">
 
-				<h2 class="woocommerce-column__title"><?php _e( 'Shipping address', 'woocommerce-subscriptions-gifting' ); ?></h2>
+			<h2 class="woocommerce-column__title"><?php esc_html_e( 'Shipping address', 'woocommerce-subscriptions-gifting' ); ?></h2>
 
-				<address>
-					<?php echo ( $address = $order->get_formatted_shipping_address() ) ? $address : __( 'N/A', 'woocommerce-subscriptions-gifting' ); ?>
-				</address>
+			<address>
+				<?php $address = $order->get_formatted_shipping_address() ? $address : __( 'N/A', 'woocommerce-subscriptions-gifting' );
+				sprintf( __( '%s', 'woocommerce-subscriptions-gifting' ), $address ); ?>
+			</address>
 
-			</div><!-- /.col-1 -->
+		</div><!-- /.col-1 -->
 
-		</section><!-- /.col1-set -->
+	</section><!-- /.col1-set -->
 
 </section>
